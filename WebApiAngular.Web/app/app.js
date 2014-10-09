df3fc280e3fc71717df074a04e9b0234364ae540
irokhes <jorgereyero@gmail.com>
2014-10-09 20:03:00 +0100

    Create SPA project

---
+++ WebApiAngular.Web/app/app.js
@@ -0,0 +1,27 @@
+﻿
+var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/app/views/orders.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
---
+++ WebApiAngular.Web/app/services/ordersService.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+app.factory('ordersService', ['$http', function($http) {
+
+    var serviceBase = 'http://localhost:19995/';
+    var orderServiceFactory = {};
+
+    var _getOrders = function() {
+        return $http.get(serviceBase + 'api/orders').then(function(results) {
+            return results;
+        });
+    };
+
+    orderServiceFactory.getOrders = _getOrders;
+
+    return orderServiceFactory;
+
+}]);
